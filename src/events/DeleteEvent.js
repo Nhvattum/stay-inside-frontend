86354fe5a9749f49196866993bb01aacfd920aeb
Nick Hvattum <nhvattum@gmail.com>
2020-08-01 17:20:05 -0700

    just changed DeleteEvent on DeleteEvent

--- src/events/DeleteEvent.js
+++ src/events/DeleteEvent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import { Button } from 'react-bootstrap'
 
-export default function DeleteComment(props) {
+export default function DeleteEvent(props) {
   const handleDelete = e => {
     // window.location.reload(); 
     e.preventDefault()
